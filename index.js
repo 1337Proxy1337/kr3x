33593d4cc04a235f9ee01e37e28c6e802d2fca5f
1337Proxy <51795741+1337Proxy1337@users.noreply.github.com>
2020-07-24 16:13:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({ disableEveryone: false });
-const botconfig = require("./botconfig.json");
+const bot = new Discord.Client({disableEveryone: true});
+const botconfig = require("./botconfig.json")
 const fs = require("fs");
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-
-// Make Bot Read Commands.
 fs.readdir("./commands/", (err, files) => {
     if(err) console.log(err);
 
     let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0) {
-        console.log("Couldn`t Find Any Commands!");
+    if(jsfile.length <= 0){
+        console.log("Couldnt find any commands!")
         return;
+    }
 
-       }
+    jsfile.forEach((f) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props);
 
-       jsfile.forEach((f) => {
-           let props = require(`./commands/${f}`)
-           console.log(`${f} loaded!`)
-           bot.commands.set(props.help.name, props);
-
-           props.help.aliases.forEach(alias => {
-               bot.aliases.set(alias, props.help.name);
-           })
-       })
-
-
+        props.help.aliases.forEach(alias =>{
+            bot.aliases.set(alias, props.help.name);
+        })
+    })
 })
 
-
-//bot activity message
-bot.on("ready", async () =>{
-    console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`)
     bot.user.setActivity(`Upgrading ${bot.guilds.cache.size} Servers!`)
-})
+});
 
-bot.on("message", async message => {
-
-    //check channel type
+bot.on("message", async message =>{
     if(message.channel.type === "dm") return;
     if(message.author.bot) return;
 
-    // find prefix
     let prefix = botconfig.prefix;
 
-    // prefix.check
     if(!message.content.startsWith(prefix)) return;
-    let args = message.content.slice(prefix.length).trim().split(/ +/g);
-    let cmd
+    let args = message.content.slice(prefix.length).trim().split(/ +/g)
+    let cmd;
     cmd = args.shift().toLowerCase();
     let command;
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot, message, args);
 
-    // CommandsFiles
-     if(bot.commands.has(cmd)) {
-         command = bot.commands.get(cmd);
-     } else if (bot.aliases.has(cmd)) {
-         command = bot.commands.get(bot.aliases.get(cmd));
-     }
-     try {
-         command.run(bot, message, args);
-     } catch (e) {
-         return;
-     }
-})
+    if(bot.commands.has(cmd)){
+        command = bot.commands.get(cmd);
+    }else if (bot.aliases.has(cmd)){
+        command = bot.commands.get(bot.aliases.get(cmd));
+    }
+    try{
+        command.run(bot, message, args);
+    } catch (e){
+        return;
+    }
+});
 
 bot.login(process.env.token);
+
+//${client.guilds.cache.size}
