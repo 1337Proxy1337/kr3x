add8245ea7a4283fef14cc1ed6559be12438f06f
1337Proxy <51795741+1337Proxy1337@users.noreply.github.com>
2020-12-26 23:59:24 +0400

    update features

--- commands/premium.js
+++ commands/premium.js
@@ -1,39 +1,31 @@
-// Modules
+    // Modules
 
-
-const Discord = require("discord.js")
-
-let premiumembed = new Discord.MessageEmbed();
-premiumembed.setTitle = "Premium checker"
-premiumembed.setColor = "#000000"
-
-// Normal Server
-
-let nopremiumembed = new Discord.MessageEmbed();
-nopremiumembed.setTitle = "Premium checker"
-nopremiumembed.setColor = "#000000"
-
-// Premium Server
-
-if(Discord.Guild.id === 732211012526276678)
-premiumembed.addField('This Server Has Premium!');
-premiumembed.addField('Premium Advantages:');
-premiumembed.addField('2x Work command payment');
-premiumembed.addField('2x Premium daily command');
-premiumembed.addField('Access to promoting ur server'); 
-message.reply(premiumembed) 
-
-//declaration or statement expected 
-
-
-} else {
-nopremiumembed.addField('This server doesnt have premium!')
-
-message.reply(nopremiumembed)
-
-
-module.exports.help = {
-    name: "ping",
-    aliases: ["p"]
-}
-}
+    module.exports.run = async (bot, message, args) => {
+    const Discord = require("discord.js")
+    
+    let premiumembed = new Discord.MessageEmbed();
+    premiumembed.setTitle = "Premium checker"
+    premiumembed.setColor = "#000000"
+    
+    // Normal Server
+    
+    let nopremiumembed = new Discord.MessageEmbed();
+    nopremiumembed.setTitle = "Premium checker"
+    nopremiumembed.setColor = "#000000"
+    
+    // Premium Server
+    
+    if(Discord.Guild.id === 732211012526276678)
+    premiumembed.addFields (
+        { name: 'This server has premium!', value: 'to see premium advantages use command k!premiumad!' },
+    )
+    message.reply(premiumembed) 
+    
+    
+    }
+    module.exports.help = {
+        name: "premium",
+        aliases: ["pre"]
+    }
+    
+    
